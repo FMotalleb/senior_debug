2a452f74be7f55932a69d7939c19b47f1e98b552
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-09-21 14:46:16 +0330

    [Minor]

--- example/senior_debug_example.dart
+++ example/senior_debug_example.dart
@@ -17,11 +17,11 @@ void main() {
   > here 2
   > here 3
   > here 4
-  > file:///home/(usr)/Documents/GitHub/senior_debug/example/senior_debug_example.dart:9:3
+  > file:///.../senior_debug/example/senior_debug_example.dart:9:3
   > 
   > here WHERE IS THIS
-  > file:///home/(usr)/Documents/GitHub/senior_debug/example/senior_debug_example.dart:10:10
+  > file:///.../example/senior_debug_example.dart:10:10
   > 
   > Test 15
-  > file:///home/motalleb/Documents/GitHub/senior_debug/example/senior_debug_example.dart:12:22
+  > file:///.../example/senior_debug_example.dart:12:22
 */
